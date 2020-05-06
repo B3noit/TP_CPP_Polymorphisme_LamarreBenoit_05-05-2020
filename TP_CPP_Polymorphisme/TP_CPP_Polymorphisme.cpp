034d0b255726eb703a1ad0875eda90a9afe17fb3
Ben <b.lamarre@xn--ludus-acadme-keb.com>
2020-05-06 10:06:48 +0200

    tout va bien
    premier test fonction affiche

--- TP_CPP_Polymorphisme/TP_CPP_Polymorphisme.cpp
+++ TP_CPP_Polymorphisme/TP_CPP_Polymorphisme.cpp
@@ -1,20 +1,31 @@
-// TP_CPP_Polymorphisme.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <iostream> 
 
-#include <iostream>
+using namespace std;
 
-int main()
+#include "CFigure.h"
+#include "Triangle.h"
+#include "Carre.h" 
+
+//  une  fonction  qui  permet  d�afficher  la  description  d�une  Figure 
+void afficherInfos(const CFigure& f)
 {
-    std::cout << "Hello World!\n";
+	cout << "Je suis un " << f.description() << endl;
+	cout << "Mes attributs sont : " << " " << f.toString() << endl;
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+int main() {
+	CTriangle triangle(5, 8); 
+	CCarre carre(4); 
+	/*cout << endl; 
+	//cout << "Triangle :" << endl; 
+	cout << "Je suis un " << triangle.description() << endl; 
+	cout << "Mes attributs sont : " << " " << triangle.toString() << endl; 
+	//cout << "Carre :" << endl;
+	cout << "Je suis un " << carre.description() << endl; 
+	cout << "Mes attributs sont : " << " " << carre.toString() << endl; cout << endl; return 0 ; */
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+	afficherInfos(triangle); // pas de souci : un triangle est une figure 
+	afficherInfos(carre); // pas de souci : un carr� est une figure 
+}
+
+
